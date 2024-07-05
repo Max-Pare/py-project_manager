730118d09c98c7e9df90be0f6ff942ff66176a59
Max-Pare <maxparegd@gmail.com>
2024-07-05 14:20:40 +0200

    Added ability to specify directory for projects

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ CPU_COUNT = multiprocessing.cpu_count()
 
 DEFAULT_CONFIG = {'entry': 'Not much to see here yet.'}
 
+work_dir = None
+config = None
+
+#TODO: should make file existence checks more consistent, either try: open or if exists: .
 if not os.path.isdir(PY_PROJMAN_CONFIG_DIR):
     os.mkdir(PY_PROJMAN_CONFIG_DIR)
 if not os.path.isfile(PY_PROJMAN_SETTINGS):
@@ -23,17 +27,25 @@ if not os.path.isfile(PY_PROJMAN_SETTINGS):
         json.dump(DEFAULT_CONFIG, file)
 if not os.path.isfile(PY_PROJMAN_CACHE):
     with open(PY_PROJMAN_CACHE, 'w') as file:
-        json.dump(set([]), file)
-
+        json.dump(list(), file)
 try:
     with open(PY_PROJMAN_CACHE, 'r') as file:
         cache = set(json.load(file))
 except FileNotFoundError as e:
-    print(f'Cache file not found at "{PY_PROJMAN_CACHE}"!!!')
-    print(e)
+    print(f'Cache file not found at "{PY_PROJMAN_CACHE}"!!!\n' + str(e))
     exit(-1)
+try:
+    with open(PY_PROJMAN_SETTINGS, 'r') as file:
+        config = dict(json.load(file))
+except FileNotFoundError as e:
+    print(f'Config file not found at "{PY_PROJMAN_SETTINGS}"!!!\n' + str(e))
+    exit(-1)
+if work_dir := config.get('work_dir') is None:
+    _raw_input = input('Enter the parent directory of your projects: ').strip()
+    if os.path.isdir(_raw_input):
+        work_dir = _raw_input    
 if not cache and input('Scan current folder for existing projects? (y/n):').lower() == 'y':
-    dirs = set(glob.iglob('./**'))
+    dirs = set(glob.iglob(f'{work_dir}/**'))
     for dir in dirs:
         if not os.path.isdir(dir): continue
         cache.add(pathlib.Path(dir).name)
